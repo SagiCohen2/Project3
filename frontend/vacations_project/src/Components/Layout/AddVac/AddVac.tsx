098771a5a18461ba82206e396a7afd842a0feda8
sagi cohen <sagicohennnn@gmail.com>
2023-04-27 13:53:57 +0200

    navigate

--- frontend/vacations_project/src/Components/Layout/AddVac/AddVac.tsx
+++ frontend/vacations_project/src/Components/Layout/AddVac/AddVac.tsx
@@ -1,10 +1,23 @@
 import "./AddVac.css";
+import { Button } from '@mui/material';
 
 function AddVac(): JSX.Element {
     return (
         <div className="AddVac">
-			<div className="Box">Add Vacation:<br/>
-            <button>Add Vacation</button>
+			<div className="Box">Add Vacation:<hr/>
+            Destination:<br/><input type="text"></input><br/>
+            <hr/>
+            Description:<br/><textarea></textarea><br/>
+            <hr/>
+            Start on:<br/><input type="date"></input>
+            <hr/>
+            End on:<br/><input type="date"></input>
+            <hr/>
+            Price:<br/><input type="number"></input>
+            <hr/>
+            Cover image:<br/><input type="file"></input><hr/>
+            <Button variant="contained">Add Vacation</Button><br/>
+            <Button variant="contained">Cancel</Button><br/>
             </div>
         </div>
     );
--- frontend/vacations_project/src/Components/Layout/EditVac/EditVac.tsx
+++ frontend/vacations_project/src/Components/Layout/EditVac/EditVac.tsx
@@ -1,10 +1,24 @@
 import "./EditVac.css";
+import { Button } from '@mui/material';
 
 function EditVac(): JSX.Element {
     return (
         <div className="EditVac">
-			<div className="Box">Edit Vacation<br/>
-            <button>Done</button></div>
+			<div className="Box">Edit Vacation:<hr/>
+            Destination:<br/><input type="text"></input><br/>
+            <hr/>
+            Description:<br/><textarea></textarea><br/>
+            <hr/>
+            Start on:<br/><input type="date"></input>
+            <hr/>
+            End on:<br/><input type="date"></input>
+            <hr/>
+            Price:<br/><input type="number"></input>
+            <hr/>
+            Cover image:<br/><input type="file"></input><hr/>
+            <Button variant="contained">Update</Button><br/>
+            <Button variant="contained">Cancel</Button><br/>
+            </div>
         </div>
     );
 }
