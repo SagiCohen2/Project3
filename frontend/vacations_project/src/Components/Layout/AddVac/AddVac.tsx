c18421d485cf755dd5e0fc79e8fdf5a02a1bea4d
sagi cohen <sagicohennnn@gmail.com>
2023-04-28 22:30:42 +0200

    add vac mui

--- frontend/vacations_project/src/Components/Layout/AddVac/AddVac.tsx
+++ frontend/vacations_project/src/Components/Layout/AddVac/AddVac.tsx
@@ -1,8 +1,9 @@
 import "./AddVac.css";
-import { Button } from '@mui/material';
+import { Button, FormControl, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import Vacation from '../../../Model/Vacation';
 import { useNavigate } from 'react-router-dom';
+import { DatePicker } from "@mui/x-date-pickers";
 
 function AddVac(): JSX.Element {
     const navigate = useNavigate();
@@ -24,17 +25,32 @@ function AddVac(): JSX.Element {
     return (
         <div className="AddVac">
             <form onSubmit={handleSubmit(send)}>
-			<div className="Box"><h3>Add Vacation:</h3><hr/>
-            Destination:<br/><input type="text" {...register("destination")}></input><br/>
+			<div className="Box"><h3>Add Vacation:</h3><br/>
+            <TextField fullWidth
+          id="outlined-multiline-flexible"
+          label="Destination"
+          multiline
+          maxRows={4} {...register("destination")}
+        />
+            <br/><hr/>
+            <TextField fullWidth
+          id="outlined-multiline-flexible"
+          label="Description"
+          multiline
+          maxRows={9} {...register("description")}
+        />
+            <br/>
             <hr/>
-            Description:<br/><textarea {...register("description")}></textarea><br/>
+            <DatePicker label="Start on:"  {...register("startDate")}/><br/>
+            <DatePicker label="End on:"  {...register("endDate")}/>
             <hr/>
-            Start on:<br/><input type="date" {...register("startDate")}></input>
-            <hr/>
-            End on:<br/><input type="date" {...register("endDate")}></input>
-            <hr/>
-            Price:<br/><input type="number" {...register("price")}></input>
-            <hr/>
+            <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Price</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount" {...register("price")}
+          /></FormControl><br/>
             Cover image:<br/><input type="file" {...register("vacImage")}></input><hr/>
             <Button fullWidth variant="contained" type="submit">Add Vacation</Button><br/>
             <Button variant="contained" color="error" size="small">Cancel</Button><br/>
